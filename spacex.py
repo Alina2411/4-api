856e929f6e2157a04b17bb3f3dcd933ef9a3adf7
Alina2411 <127033529+Alina2411@users.noreply.github.com>
2024-04-02 17:27:08 +0300

    Update spacex.py

--- spacex.py
+++ spacex.py
@@ -1,17 +1,30 @@
 import requests
-from downloads_images import downloads_images
+import argparse
+from download_images import download_images
 
-def fetch_spacex_last_launch():
-  url = "https://api.spacexdata.com/v5/launches/5eb87d47ffd86e000604b38a"
-  response = requests.get(url)
-  links = response.json()['links']['flickr']['original']
-  for number_link, link in enumerate(links):
-      file_name = f'images/spacex{number_link}.jpg'
-      downloads_images(file_name, link)
+
+def fetch_spacex_last_launch(launch_id):
+    url = f"https://api.spacexdata.com/v5/launches/{launch_id}"
+    response = requests.get(url)
+    links = response.json()['links']['flickr']['original']
+    for number_link, link in enumerate(links):
+        file_name = f'images/spacex{number_link}.jpg'
+        download_images(file_name, link)
 
 
 def main():
-    fetch_spacex_last_launch()
+    parser = argparse.ArgumentParser(
+      description='Эта программа позволит вам скачать фотографии с запуска SpaceX.'
+    )
+    parser.add_argument(
+      '--id',
+      dest='launch_id',
+      default='5eb87d47ffd86e000604b38a',
+      help='Укажите ID запуска SpaceX, с которого можно загрузить фотографии.'
+    )
+    args = parser.parse_args()
+    fetch_spacex_last_launch(args.launch_id)
+  
 
 if __name__ == '__main__':
     main()

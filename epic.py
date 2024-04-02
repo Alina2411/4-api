a458759cd4943a1325024e6d0ee0db7d86c1f172
Alina2411 <127033529+Alina2411@users.noreply.github.com>
2024-04-02 17:25:31 +0300

    Update epic.py

--- epic.py
+++ epic.py
@@ -1,14 +1,15 @@
 import requests
 import os
 import datetime
-from downloads_images import downloads_images
+from download_images import download_images
 
-def fetch_nasa_epic():
-    nasa_token = '2pzXEr6S459lIrdI9Ndw0oz5MAFnt2aFfCwuMit2'
+
+def fetch_nasa_epic(nasa_token):
     nasa_epic_url = 'https://api.nasa.gov/EPIC/api/natural/image'
+    count = 30
     params ={
         'api_key': nasa_token,
-        'count': 30
+        'count': count
     }
     response = requests.get(nasa_epic_url, params=params)
     epic_image = response.json()
@@ -18,11 +19,13 @@ def fetch_nasa_epic():
       epic_date = datetime.datetime.fromisoformat(epic_date).strftime('%Y/%m/%d')                
       epic_url= f'https://api.nasa.gov/EPIC/archive/natural/{epic_date}/png/{epic_name}.png'
       path = os.path.join('images',f'{epic_name}.png')
-      downloads_images(path, epic_url, params)
+      download_images(path, epic_url, params)
 
 
 def main():
-    fetch_nasa_epic()
+    nasa_token = os.environ['NASA_TOKEN']
+    fetch_nasa_epic(nasa_token)
+
 
 if __name__ == '__main__':
     main()

b7feed8e9a04028d8466eaa9be0135af460720d9
Alina2411 <127033529+Alina2411@users.noreply.github.com>
2024-04-02 17:28:20 +0300

    Update and rename main.py to upload_images.py

---
+++ upload_images.py
@@ -0,0 +1,22 @@
+import telegram
+import os
+import random
+from time import sleep
+
+
+def main():
+    tg_token = os.environ['TG_TOKEN']
+    chat_id = os.environ['TG_CHAT_ID']
+    bot = telegram.Bot(token=tg_token)
+    while True:
+      files = os.listdir("images")
+      random.shuffle(files)
+      for file in files:
+        filepath = os.path.join("images", file)
+        with open(filepath, 'rb') as f:
+            bot.send_document(chat_id=chat_id, document=f)
+            sleep(10)
+
+
+if __name__ == '__main__':
+  main()
